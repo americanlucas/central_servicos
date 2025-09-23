38b9fc0e992d13709eb0b958a41466dad05c3316
americanlucas <lucasamericano.dev@gmail.com>
2025-09-22 21:03:38 -0300

    Initial Commit

---
+++ database/test/tipo_ocorrencia_test.py
@@ -0,0 +1,34 @@
+from database.tipo_ocorrencia_dao import TipoOcorrenciaDAO
+
+def incluir ():
+    nome = input('Tipo ocorrencia: ')
+    texto = input('Tipo ocorrencia: ')
+    dao = TipoOcorrenciaDAO()
+    to = dao.new_object()
+    to.nme_tipo_ocorrencia = nome
+    to.txt_modelo_ocorrencia = texto
+    to.tpo_tipo_ocorrencia = 'E'
+    to.sts_tipo_ocorrencia = 'A'
+    dao.insert(to)
+    print(f'Inserido ocorrência número {to.idt_tipo_ocorrencia}')
+
+def listar ():
+    dao = TipoOcorrenciaDAO()
+    lista = dao.read_all()
+    for to in lista:
+        print(to.idt_tipo_ocorrencia, ' - ', to.nme_tipo_ocorrencia, ' - ', to.txt_tipo_ocorrencia)
+
+if __name__ == '__main__':
+    while True:
+        opc = int(input('''
+            1 - Incluir
+            2 - Listar
+            3 - Sair
+        '''))
+
+        if opc == 1:
+            incluir()
+        elif opc == 2:
+            listar()
+        else:
+            break
